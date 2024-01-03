ecaa52f5438f5d4fa93aabd47f877e2da8ad76f3
jvargas2319 <j.vargas2319@gmail.com>
2024-01-03 03:17:05 -0800

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+
+export default function Home() {
+  const createUserAction =  async(formData: FormData) => {
+    "use server";
+
+    const name = formData.get("name");
+    console.log({name});
+
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-500">
+            Enter Dog Name
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action={createUserAction} method="POST">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-indigo-400"
+              >
+                Dog Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  
+                  required
+                  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          
+        </div>
+      </div>
+    </main>
+  );
+}

1d483269214ed4f23dfe63ce1326a03801d9befa
jvargas2319 <j.vargas2319@gmail.com>
2024-01-04 03:08:16 -0800

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   const createUserAction =  async(formData: FormData) => {
-    "use server";
+    "use server"; 
 
     const name = formData.get("name");
     console.log({name});
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-500">
-            Enter Dog Name hi
+            Enter Dog Name hiee
           </h2>
         </div>
 

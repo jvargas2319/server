e24bca6d718fd8072729af553dbab933e8a9cd08
jvargas2319 <j.vargas2319@gmail.com>
2024-01-04 02:20:40 -0800

    intial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-500">
-            Enter Dog Name
+            Enter Dog Name hi
           </h2>
         </div>
 
